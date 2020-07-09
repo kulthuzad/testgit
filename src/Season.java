5361b58c26fa582f3816c4d40dda63302c964ddd
kulthuzad <LZX479670824@126.com>
2020-07-09 14:01:43 +0800

    first

---
+++ src/Season.java
@@ -0,0 +1,20 @@
+
+
+
+	
+	public enum Season{
+		
+		SPRING("春天","zero"),
+		SUMMER("xiatian","one"),
+		AUTUMN("qiutian","three"),
+		WINTER("dongtian","four");
+		
+		
+		
+		
+		
+		private final String name;
+		private final String desc;
+	}
+	
+
